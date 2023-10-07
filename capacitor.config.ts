58292a9b88b69bb446aeb9288f930e785c2ed9db
Jeremias Pajaro <jeremias.pajaro@bitsamericas.com>
2023-10-07 11:28:14 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'prueba_pragma',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
